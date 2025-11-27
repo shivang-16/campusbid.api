8a510b9687a08f26e88d8df756b8d37e679c559c
Shivang Yadav <125182653+shivang-16@users.noreply.github.com>
2025-11-27 18:37:16 +0530

    Log database URL and check for its existence
    Added a console log for the database URL and added a check for its existence.

--- src/db/db.ts
+++ src/db/db.ts
@@ -7,6 +7,8 @@ let db: mongoose.Connection;
 
 const ConnectToDB = async () => {
   const DatabaseUrl = process.env.DATABASE_URL as string;
+  console.log(DatabaseUrl);
+  if(!DatabaseUrl) returnl
 
   try {
     await mongoose.connect(DatabaseUrl);
